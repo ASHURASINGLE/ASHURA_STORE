f9a76dc2f840d36f5b795dea7a7c75f34b65343b
ASHURASINGLE <darkness789867@gmail.com>
2025-04-06 22:51:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function toggleDrawer() {
   const drawer = document.getElementById("drawer");
-  drawer.style.left = drawer.style.left === "0px" ? "-250px" : "0px";
+  if (drawer.style.left === "0px") {
+    drawer.style.left = "-200px";
+  } else {
+    drawer.style.left = "0px";
+  }
 }
 
-function createGlow(e) {
-  const glow = document.createElement("span");
-  glow.classList.add("click-glow");
-  glow.style.left = `${e.clientX - 30}px`;
-  glow.style.top = `${e.clientY - 30}px`;
-  document.body.appendChild(glow);
-
+function glowButton() {
+  const btn = document.querySelector(".start-button");
+  btn.style.boxShadow = "0 0 30px red";
   setTimeout(() => {
-    glow.remove();
-  }, 500);
+    btn.style.boxShadow = "0 0 10px red";
+  }, 300);
 }
