2095fae95307f52cc7f313b8a5ac297a85c1bc79
ASHURASINGLE <darkness789867@gmail.com>
2025-04-06 21:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function toggleDrawer() {
-  const drawer = document.getElementById("drawer");
-  drawer.classList.toggle("open");
-}
+const images = [
+  'https://wallpapercave.com/wp/wp8449990.jpg',
+  'https://wallpapercave.com/wp/wp9079152.jpg',
+  'https://wallpapercave.com/wp/wp8841650.jpg',
+  'https://wallpapercave.com/wp/wp7370870.jpg',
+  'https://wallpapercave.com/wp/wp12385320.jpg'
+];
+
+window.onload = () => {
+  const hero = document.getElementById('hero');
+  const bg = images[Math.floor(Math.random() * images.length)];
+  hero.style.backgroundImage = `url('${bg}')`;
+};
 
 function startApp() {
-  const btn = document.querySelector("button");
-  btn.innerText = "Loading...";
-  btn.disabled = true;
+  const button = document.querySelector('button');
+  button.innerText = 'Loading...';
   setTimeout(() => {
-    window.location.href = "login.html";
+    window.location.href = 'login.html';
   }, 1000);
+}
+
+function toggleDrawer() {
+  const drawer = document.getElementById('drawer');
+  drawer.classList.toggle('open');
 }
